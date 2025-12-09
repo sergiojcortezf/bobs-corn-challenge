9b0a119f4bb86bb785857016516fe8e65261810f
Sergio.Cortez <sergiojcortezf@gmail.com>
2025-12-09 15:56:06 -0600

    test(api): add automated tests for rate limiting business rules

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.core.cache import cache
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Transaction
 
-# Create your tests here.
+class BuyCornTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('buy-corn')
+
+    def tearDown(self):
+        cache.clear()
+
+    def test_buy_corn_success(self):
+        """
+        Prueba que un usuario puede comprar una mazorca exitosamente.
+        """
+        response = self.client.post(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], "Enjoy your corn! 🌽")
+        self.assertEqual(Transaction.objects.count(), 1)
+
+    def test_rate_limit_blocking(self):
+        """
+        Prueba que el sistema bloquea al usuario si intenta comprar
+        dos veces en menos de un minuto.
+        """
+        response1 = self.client.post(self.url)
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+
+        response2 = self.client.post(self.url)
+        self.assertEqual(response2.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
+        
+        self.assertEqual(Transaction.objects.count(), 1)
