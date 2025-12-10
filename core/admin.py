41d1b4f2fde8b9af82b132b57902713a04aa88d5
Sergio.Cortez <sergiojcortezf@gmail.com>
2025-12-09 23:05:18 -0600

    feat(admin): configure read-only admin interface for auditing transactions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('client_ip', 'amount', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('client_ip',)
+    readonly_fields = ('id', 'client_ip', 'amount', 'created_at')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
